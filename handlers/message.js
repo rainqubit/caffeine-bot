9ffb8af7a46e8000c3043f34dfea827e8093ca1c
Unknown <riyandanin010699@gmail.com>
2018-09-06 09:58:09 +0700

    Added spam detector

--- handlers/message.js
+++ handlers/message.js
@@ -1,7 +1,21 @@
+// Require spam detector
+const spam = require("spamnya")
 // Fired when a message is sent
 function handle(client, message, _) {
   if (message.author.bot) 
     return;
+
+  //initiate spam detector with 50 max logged chats
+  spam.log(message, 50)
+  if(spam.tooQuick(2, 1000)){
+    //when someone send 2 chats in less than a second
+    message.channel.send("slow down") //example
+  }
+
+  if(spam.sameMessages(3)){
+    //when someone sends 3 identical messages
+    message.channel.send("stop spamming") //example
+  }
 
   if (message.content.startsWith(client.config.prefix)) {
     // The message is a command
